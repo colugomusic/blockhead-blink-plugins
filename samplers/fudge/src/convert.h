96ebc0a71ff4a6284e1775d372f712e708320059
Colugo <colugomusic@gmail.com>
2026-03-30 14:01:44 +0100

    Update convert.h

--- samplers/fudge/src/convert.h
+++ samplers/fudge/src/convert.h
@@ -3,26 +3,23 @@
 #include <cmath>
 #include <blink/math.hpp>
 #include <blink/quiet-include.hpp>
+#include <snd/misc.hpp>
 BLINK_QUIET_INCLUDE_BEGIN
 #include <DSP/MLDSPOps.h>
 BLINK_QUIET_INCLUDE_END
 
 namespace fudge_sampler {
 namespace convert {
-
 constexpr auto GRAIN_SIZE_MIN_MS = 1.0f;
 constexpr auto GRAIN_SIZE_MAX_MS = 1000.0f;
-
 [[nodiscard]] inline
 auto linear_to_ms(float linear) -> float {
-	return blink::math::lerp(GRAIN_SIZE_MIN_MS, GRAIN_SIZE_MAX_MS, std::pow(linear, 4.0f));
+	return std::lerp(GRAIN_SIZE_MIN_MS, GRAIN_SIZE_MAX_MS, std::pow(linear, 4.0f));
 }
-
 [[nodiscard]] inline
 auto ms_to_linear(float ms) -> float {
-	return std::pow(blink::math::inverse_lerp(GRAIN_SIZE_MIN_MS, GRAIN_SIZE_MAX_MS, ms), 0.25f);
+	return std::pow(snd::inverse_lerp(GRAIN_SIZE_MIN_MS, GRAIN_SIZE_MAX_MS, ms), 0.25f);
 }
-
 [[nodiscard]] inline
 auto linear_to_ms(const ml::DSPVector& linear) -> ml::DSPVector {
 	return ml::select(
@@ -30,17 +27,13 @@ auto linear_to_ms(const ml::DSPVector& linear) -> ml::DSPVector {
 		ml::lerp(ml::DSPVector(GRAIN_SIZE_MIN_MS), ml::DSPVector(GRAIN_SIZE_MAX_MS), ml::pow(linear, ml::DSPVector(4.0f))),
 		ml::lessThanOrEqual(linear, ml::DSPVector(0.0f)));
 }
-
 [[nodiscard]] inline
 auto ms_to_linear(const ml::DSPVector& ms) -> ml::DSPVector {
 	return ml::pow(ml::inverseLerp(ml::DSPVector(GRAIN_SIZE_MIN_MS), ml::DSPVector(GRAIN_SIZE_MAX_MS), ms), ml::DSPVector(0.25f));
 }
-
 [[nodiscard]] inline
 auto ms_to_samples(const ml::DSPVector& ms, int SR) -> ml::DSPVector {
 	return (ms / GRAIN_SIZE_MAX_MS) * float(SR);
 }
-
 } // namespace convert
 } // namespace fudge_sampler
-
